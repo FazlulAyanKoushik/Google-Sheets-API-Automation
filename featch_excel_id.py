89c6f209ae4cfbbb8a778260716d16b1b1bf0168
FazlulAyanKoushik <ak.fazlulayan177@gmail.com>
2024-09-27 16:55:10 +0600

    Improve spacing in extract_id_from_link function

--- featch_excel_id.py
+++ featch_excel_id.py
@@ -1,12 +1,13 @@
 import re
 
+
 def extract_id_from_link(link):
     # Regular expression pattern to extract the ID from the Google Sheets link
     pattern = r'/spreadsheets/d/([a-zA-Z0-9-_]+)'
-    
+
     # Use re.search to find the pattern in the link
     match = re.search(pattern, link)
-    
+
     # If a match is found, return the ID group, otherwise return None
     if match:
         return match.group(1)
